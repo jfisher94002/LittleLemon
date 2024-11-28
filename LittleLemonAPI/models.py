e39e25f15f3d1fdf82a6975713b68b1b298e088f
Jonathan Fisher <jfisher94002@gmail.com>
2024-11-27 23:22:50 -0800

    Update after going through exam questions

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
-	slug = models.SlugField()	
-	title = models.CharField(max_length=255)
-	def __str__(self) -> str:
-		return self.title
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
 class MenuItem(models.Model):
 	title = models.CharField(max_length=255, db_index=True)
 	price = models.DecimalField(max_digits=6, decimal_places=2)
